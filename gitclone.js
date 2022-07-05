3fef3931596ef3fec85195d6a477c7d0ac78e75d
Domenico Monaco <82922+domenicomonaco@users.noreply.github.com>
2022-07-05 16:41:42 +0200

    no message

--- gitclone.js
+++ gitclone.js
@@ -2,6 +2,7 @@ const shell = require('shelljs')
 const fs = require('fs');
 const csv = require('csv-parser');
 const dotenv = require("dotenv");
+const prompts = require('prompts');
 
 //LOAD ENNV
 dotenv.config();
@@ -10,49 +11,61 @@ function gitClone(ex) {
   const baseURL = process.env.BASEGITURL;
   const basefolder = process.env.BASEFOLDER;
 
-  if (fs.existsSync(basefolder)) {
+  //if (fs.existsSync(basefolder)) {
 
     fs.createReadStream('users.csv')
       .pipe(csv())
       .on('data', (row) => {
-        if (fs.existsSync(basefolder)==false){
+
+        console.log('------------------------------------------------------------------- \n');
+        console.log(row['nome'], row['cognome'], '@' + row['gitusername']);
+
+        if (fs.existsSync(basefolder) == false) {
           shell.mkdir('-p', basefolder);
         }
         shell.cd(basefolder);
 
-        console.log('------------------------------------------------------------------- \n');
-        console.log(row['nome'], row['cognome'],'@'+row['gitusername']);
-        //console.log(row);
-
-        if (fs.existsSync(row['gitusername'])) {
-          shell.cd(row['gitusername']);
-          if (fs.existsSync(ex)) {
-            shell.cd(ex);
-
-            shell.exec('git reset --hard HEAD');
-            shell.exec('git pull --force');
-
-            shell.cd('..');
-
-          } else {
-            shell.exec(baseURL.toString() + row['gitusername'] + '/' + ex + '.git');
-          }
-          shell.cd('..');
-        }
-        else {
+        if (fs.existsSync(row['gitusername']) == false) {
           shell.mkdir('-p', row['gitusername']);
         }
-        shell.cd('..');
+        shell.cd(row['gitusername']);
+
+        if (fs.existsSync(ex)) {
+          shell.cd(ex);
+
+          shell.exec('git reset --hard HEAD');
+          shell.exec('git pull --force');
+
+          shell.cd('..');
+
+        } else {
+          shell.exec(baseURL.toString() + row['gitusername'] + '/' + ex + '.git');
+        }
+    
+        shell.cd('../..');
+    
       })
       .on('end', () => {
         console.log('CSV file successfully processed');
       });
 
+  //}
+
+}
+
+(async () => {
+  const response = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'Quanti cicli vuoi fare?',
+    initial:1,
+    validate: value => value < 1 ? `deve essere almeno 1` : true
+  });
+
+  for (let i = 0; i <= parseInt(response['value']); i++) {
+    gitClone(process.env.REPONAME);
   }
 
-}
+  
+})();
 
-
-for (let i = 1; i <process.env.LOOPN;i++) {
-  gitClone(process.env.REPONAME);
-}
